b36f5077accf623ee36505632fec74ccf134fd89
Greg Murray <gremurra@gmail.com>
2020-04-02 17:16:44 -0400

    first commit

---
+++ BlackjackChallenge_Repo/BlackJack_Classes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackChallenge_Repo
+{
+    //PLAYER CLASS
+    public class Player
+    {
+        public int HandValue { get; set; }
+        public bool Stand { get; set; }
+        public int Turn { get; set; }
+
+        public Player(int handvalue, bool stand)
+        {
+            HandValue = handvalue;
+            Stand = stand;
+            Turn = 0;
+        }
+    }
+
+    //DEALER CLASS
+    public class Dealer
+    {
+        public int OpenValue { get; set; }
+        public int HidValue { get; set; }
+        public bool Stand { get; set; }
+
+        public Dealer(int openvalue, int hidvalue, bool stand)
+        {
+            OpenValue = openvalue;
+            HidValue = hidvalue;
+            Stand = stand;
+        }
+    }
+
+    public class GameInfo
+    {
+        public bool Active { get; set; }
+
+        public GameInfo(bool active)
+        {
+            Active = active;
+        }
+    }
+}
+
+    
